bc7ef32bb0e96ab06623d385bf76a35334468165
Alexandru Stanciu <alexandru.stanciu@gmail.com>
2015-01-15 18:39:25 +0100

    Code cleanup

--- summarizer/__init__.py
+++ summarizer/__init__.py
@@ -11,8 +11,8 @@ database.config.sqlalchemy_url = config.sqlalchemy_url
 database.config.SQLALCHEMY_DATABASE_URI = config.SQLALCHEMY_DATABASE_URI
 
 
-from redis import Redis
-r = Redis(config.REDIS_HOST, config.REDIS_PORT)
+import redis
+r = redis.Redis(config.REDIS_HOST, config.REDIS_PORT)
 
 
 import summary
