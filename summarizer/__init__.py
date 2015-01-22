039c380a8ea2f693ded1cbd17dd2c5c8ed1794c6
Alexandru Stanciu <alexandru.stanciu@gmail.com>
2015-01-22 21:21:43 +0100

    Config uniformity

--- summarizer/__init__.py
+++ summarizer/__init__.py
@@ -3,22 +3,26 @@ Summarizer initialization.
 """
 import config
 
-import database
-db = database.db
+import database, redis, summary, aggregator
 
-# TODO: Make this generic
+## Database
 database.config.sqlalchemy_url = config.sqlalchemy_url
 database.config.SQLALCHEMY_DATABASE_URI = config.SQLALCHEMY_DATABASE_URI
+db = database.db
 
+## Redis
+r = redis.StrictRedis(config.RQ_REDIS_HOST, config.RQ_REDIS_PORT, config.RQ_REDIS_DB)
 
-import redis
-r = redis.StrictRedis(config.REDIS_HOST, config.REDIS_PORT)
+## Summary
+summary.config.USER_AGENT = config.SUMMARY_USER_AGENT
+summary.config.ADBLOCK_EASYLIST_URL = config.SUMMARY_ADBLOCK_EASYLIST_URL
+summary.config.ADBLOCK_EXTRALIST_URL = config.SUMMARY_ADBLOCK_EXTRALIST_URL
 
-
-import summary
-# from summary import Summary
-
-# TODO: Make this generic
-# summary.config.USER_AGENT = config.SUMMARY_USER_AGENT
-# summary.config.ADBLOCK_EASYLIST_URL = config.SUMMARY_ADBLOCK_EASYLIST_URL
-# summary.config.ADBLOCK_EXTRALIST_URL = config.SUMMARY_ADBLOCK_EXTRALIST_URL
+## Aggregator
+aggregator.config.sqlalchemy_url = config.sqlalchemy_url
+aggregator.config.SQLALCHEMY_DATABASE_URI = config.SQLALCHEMY_DATABASE_URI
+aggregator.config.REDIS_HOST = config.AGGREGATOR_REDIS_HOST
+aggregator.config.REDIS_PORT = config.AGGREGATOR_REDIS_PORT
+aggregator.config.REDIS_DB = config.AGGREGATOR_REDIS_DB
+aggregator.config.BASE_UXTIME = config.AGGREGATOR_BASE_UXTIME
+aggregator.load_config() # delayed init
--- summarizer/config.py
+++ summarizer/config.py
@@ -7,17 +7,22 @@ Config settings for summarizer app.
 # SQLALCHEMY_ECHO = sqlalchemy_echo = True
 SQLALCHEMY_DATABASE_URI = sqlalchemy_url = 'postgresql://svven@localhost/svven'
 
-## Redis
-REDIS_HOST = 'localhost'
-REDIS_PORT = 6379
-# REDIS_DB = 0
-
-## Queues
+## RQ (Redis Queue)
+RQ_REDIS_HOST = 'localhost'
+RQ_REDIS_PORT = 6379
+RQ_REDIS_DB = 0
 QUEUES = (SUMMARIZER_QUEUE, ) = ("summarizer", )
 
 ## Summary
-# SUMMARY_USER_AGENT = 'summary-extraction 0.2'
-SUMMARY_ADBLOCK_EASYLIST_URL = 'easylist.txt' and \
-	'https://easylist-downloads.adblockplus.org/easylist.txt' #
-SUMMARY_ADBLOCK_EXTRALIST_URL = 'extralist.txt' and \
-	'https://dl.dropboxusercontent.com/u/134594/svven/extralist.txt' #
+SUMMARY_USER_AGENT = 'Svven-Summarizer'
+SUMMARY_ADBLOCK_EASYLIST_URL = 'easylist.txt' # offline
+	# 'https://easylist-downloads.adblockplus.org/easylist.txt' #
+SUMMARY_ADBLOCK_EXTRALIST_URL = 'extralist.txt' # offline
+	# 'https://dl.dropboxusercontent.com/u/134594/svven/extralist.txt' #
+
+## Aggregator
+AGGREGATOR_REDIS_HOST = 'localhost'
+AGGREGATOR_REDIS_PORT = 6379
+AGGREGATOR_REDIS_DB = 1
+
+AGGREGATOR_BASE_UXTIME = 1388534400 # datetime(2014, 1, 1, 0, 0)
