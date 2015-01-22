793d0220012a6d06647e067717c7a92cf8bae6af
Alexandru Stanciu <alexandru.stanciu@gmail.com>
2015-01-23 00:21:27 +0100

    Update aggregator config automatically

--- summarizer/__init__.py
+++ summarizer/__init__.py
@@ -19,10 +19,10 @@ summary.config.ADBLOCK_EASYLIST_URL = config.SUMMARY_ADBLOCK_EASYLIST_URL
 summary.config.ADBLOCK_EXTRALIST_URL = config.SUMMARY_ADBLOCK_EXTRALIST_URL
 
 ## Aggregator
-aggregator.config.sqlalchemy_url = config.sqlalchemy_url
-aggregator.config.SQLALCHEMY_DATABASE_URI = config.SQLALCHEMY_DATABASE_URI
-aggregator.config.REDIS_HOST = config.AGGREGATOR_REDIS_HOST
-aggregator.config.REDIS_PORT = config.AGGREGATOR_REDIS_PORT
-aggregator.config.REDIS_DB = config.AGGREGATOR_REDIS_DB
-aggregator.config.BASE_UXTIME = config.AGGREGATOR_BASE_UXTIME
-aggregator.load_config() # delayed init
+# aggregator.config.sqlalchemy_url = config.sqlalchemy_url
+# aggregator.config.SQLALCHEMY_DATABASE_URI = config.SQLALCHEMY_DATABASE_URI
+# aggregator.config.REDIS_HOST = config.AGGREGATOR_REDIS_HOST
+# aggregator.config.REDIS_PORT = config.AGGREGATOR_REDIS_PORT
+# aggregator.config.REDIS_DB = config.AGGREGATOR_REDIS_DB
+# aggregator.config.BASE_UXTIME = config.AGGREGATOR_BASE_UXTIME
+aggregator.load_config(config) # delayed init
