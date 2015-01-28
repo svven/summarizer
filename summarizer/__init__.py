c7b8f343966e496c625a3cecfd64739811d528b1
Alexandru Stanciu <alexandru.stanciu@gmail.com>
2015-01-28 21:13:30 +0200

    Load database config

--- summarizer/__init__.py
+++ summarizer/__init__.py
@@ -4,8 +4,7 @@ Summarizer initialization.
 import config, database, redis, summary, aggregator
 
 ## Database
-database.config.sqlalchemy_url = config.sqlalchemy_url
-database.config.SQLALCHEMY_DATABASE_URI = config.SQLALCHEMY_DATABASE_URI
+database.load_config(config)
 db = database.db
 
 ## Redis
@@ -17,10 +16,4 @@ summary.config.ADBLOCK_EASYLIST_URL = config.SUMMARY_ADBLOCK_EASYLIST_URL
 summary.config.ADBLOCK_EXTRALIST_URL = config.SUMMARY_ADBLOCK_EXTRALIST_URL
 
 ## Aggregator
-# aggregator.config.sqlalchemy_url = config.sqlalchemy_url
-# aggregator.config.SQLALCHEMY_DATABASE_URI = config.SQLALCHEMY_DATABASE_URI
-# aggregator.config.REDIS_HOST = config.AGGREGATOR_REDIS_HOST
-# aggregator.config.REDIS_PORT = config.AGGREGATOR_REDIS_PORT
-# aggregator.config.REDIS_DB = config.AGGREGATOR_REDIS_DB
-# aggregator.config.BASE_UXTIME = config.AGGREGATOR_BASE_UXTIME
 aggregator.load_config(config) # delayed init
