93e93ba6cd9963bde485399b90aac9128105b0e7
Alexandru Stanciu <alexandru.stanciu@gmail.com>
2015-02-03 08:56:53 +0100

    Preparing the manager

--- summarizer/__init__.py
+++ summarizer/__init__.py
@@ -11,9 +11,7 @@ db = database.db
 r = redis.StrictRedis(config.RQ_REDIS_HOST, config.RQ_REDIS_PORT, config.RQ_REDIS_DB)
 
 ## Summary
-summary.config.USER_AGENT = config.SUMMARY_USER_AGENT
-summary.config.ADBLOCK_EASYLIST_URL = config.SUMMARY_ADBLOCK_EASYLIST_URL
-summary.config.ADBLOCK_EXTRALIST_URL = config.SUMMARY_ADBLOCK_EXTRALIST_URL
+summary.config.from_object(config)
 
 ## Aggregator
 aggregator.init(config) # delayed init
---
+++ summarizer/manager.py
@@ -0,0 +1,3 @@
+"""
+Summarizer manager.
+"""
