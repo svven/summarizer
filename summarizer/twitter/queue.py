499581efe5108181087df7cfa7461c8563d3dc61
Alexandru Stanciu <alexandru.stanciu@gmail.com>
2015-07-11 10:22:32 +0300

    Finally close session

--- summarizer/twitter/queue.py
+++ summarizer/twitter/queue.py
@@ -38,9 +38,12 @@ def process(status_id):
             exc_info=True, extra={'data': {'id': status.id, 'url': status.url}})
         raise
     finally:
-        status = session.merge(status) # no need
-        status.state = failed and State.FAIL or State.DONE
-        session.commit()
+        try:
+            status = session.merge(status) # no need
+            status.state = failed and State.FAIL or State.DONE
+            session.commit()
+        except:
+            session.rollback()
         session.close()
         logger.debug("End process")
 
